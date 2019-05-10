4497e43a0d3da2e02a0b8d195c79fd890b003577
____marcell <0000marcell@gmail.com>
2019-05-10 13:48:27 +0000

    update

--- figmafetch.js
+++ figmafetch.js
@@ -3,30 +3,31 @@
 const fetch = require('node-fetch');
 const headers = new fetch.Headers();
 const program = require('commander');
+const fs = require('fs');
 
 const baseUrl = 'https://api.figma.com';
 
-async function fetchProject(baseUrl, projectId, token) {
+async function fetchProject(projectId, token) {
   headers.set('X-Figma-Token', token);
   let resp = await fetch(`${baseUrl}/v1/files/${projectId}`, 
     {headers});
   let data = await resp.json();
-  data['headers'] = headers;
   return data;
 }
 
 program
   .description('fetch figma project')
-  .option('-u, --url <url>', 'figma api url')
   .option('-i, --id <id>', 'project id')
   .option('-t, --token <token>', 'user token')
   .action(async function (cmd) {
-    console.log(cmd['url'], 
-      cmd['id'], cmd['token']);
-    data = await fetchProject(cmd['url'], 
-      cmd['id'], cmd['token']);
-    data = data.document;
-    console.log(data);
+    let token = cmd['token'] || process.env['FIGMA_TOKEN'],
+      id = cmd['id'] || fs.readFileSync('./figmaconfig', 'utf8')
+                          .replace('\n', '');
+    
+    data = await fetchProject(id, token);
+    data = data;
+    data['headers'] = { token: token, id: id };
+    console.log(JSON.stringify(data));
   });
 
 program.parse(process.argv);
